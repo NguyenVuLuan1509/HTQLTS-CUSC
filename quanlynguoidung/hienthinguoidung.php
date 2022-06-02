a95ad3ad52c3d6a1eaab724e39c80f017742b57d
Luannguyen <luannguyenhttt@gmail.com>
2022-06-02 09:23:55 +0700

    Vuluan them chuc nang hienthinguoidung

---
+++ quanlynguoidung/hienthinguoidung.php
@@ -0,0 +1,28 @@
+<div class="col-md-9 mainWeb">
+    <table class="table table-striped table-hover">
+        <tr>
+            <th>STT</th>
+            <th>SDT</th>
+            <th>Họ tên</th>
+            <th>Email</th>
+            <th>Mã Trường</th>
+            <th>Mã Nghề nghiệp</th>
+            <th>Hình thức thu thập</th>
+            <th>SDT ba</th>
+            <th>SDT mẹ</th>
+        </tr>
+
+        <tr>
+            <td>01</td>
+            <td>0326551974</td>
+            <td>Nguyễn Vũ Luân</td>
+            <td>luannguyenhttt@gmail.com</td>
+            <td>TR004</td>
+            <td>NGHE01</td>
+            <td>H01</td>
+            <td>032665495</td>
+            <td>853525353</td>
+        </tr>
+        
+    </table>
+</div>
